8bea1b0f6a89438d9a75336d40857cffb2024f27
pedrolino25 <pedrolino25@gmail.com>
2025-01-13 23:30:47 +0000

    manage service categories

--- src/app/reference-data/services-categories/page.tsx
+++ src/app/reference-data/services-categories/page.tsx
@@ -1,5 +1,32 @@
 'use client'
 
+import { Navbar } from '@/components/navigation/Navbar'
+import { getServicesCategoriesList } from '@/services/api/reference-data'
+import { useQuery } from '@tanstack/react-query'
+import dynamic from 'next/dynamic'
+const ServiceCategoriesListSection = dynamic(
+  () =>
+    import(
+      '@/components/sections/services-categories/ServiceCategoriesListSection'
+    ),
+  { ssr: false }
+)
+
 export default function ReferenceData() {
-  return <div />
+  const { isPending, data, refetch } = useQuery({
+    queryKey: ['services-categories'],
+    queryFn: async () => {
+      return await getServicesCategoriesList()
+    },
+  })
+
+  return (
+    <Navbar>
+      <ServiceCategoriesListSection
+        data={data}
+        isPending={isPending}
+        refresh={refetch}
+      />
+    </Navbar>
+  )
 }
