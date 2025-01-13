e13535e5039d9d13a3ab61b3de568d598eb8b952
pedrolino25 <pedrolino25@gmail.com>
2025-01-13 22:42:40 +0000

    manage space types

--- src/app/reference-data/space-types/page.tsx
+++ src/app/reference-data/space-types/page.tsx
@@ -1,5 +1,29 @@
 'use client'
 
+import { Navbar } from '@/components/navigation/Navbar'
+import { getSpaceTypesList } from '@/services/api/reference-data'
+import { useQuery } from '@tanstack/react-query'
+import dynamic from 'next/dynamic'
+const SpaceTypesListSection = dynamic(
+  () => import('@/components/sections/space-types/SpaceTypesListSection'),
+  { ssr: false }
+)
+
 export default function ReferenceData() {
-  return <div />
+  const { isPending, data, refetch } = useQuery({
+    queryKey: ['space-types'],
+    queryFn: async () => {
+      return await getSpaceTypesList()
+    },
+  })
+
+  return (
+    <Navbar>
+      <SpaceTypesListSection
+        data={data}
+        isPending={isPending}
+        refresh={refetch}
+      />
+    </Navbar>
+  )
 }
